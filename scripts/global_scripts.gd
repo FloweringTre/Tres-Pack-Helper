49461a20cb305cbd67bb996a3e124c02aefc0902
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-12-07 23:19:45 -0700

    added instruction and text clean functions

--- scripts/global_scripts.gd
+++ scripts/global_scripts.gd
@@ -1,12 +1,14 @@
 extends Node
 
-var directory_root : String
+var directory_root : String = OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)
 var root : String = ""
 var folder: String = ""
 var jsons_root : String
 var textures_root : String
 var report_file_path : String
-
+var instructions_file_path : String
+var coats_setup : bool = false
+var tack_setup : bool = false
 
 enum TackTypes {
 	SaddleBlanket, 
@@ -21,7 +23,7 @@ enum TackTypes {
 	SaddleBag
 }
 
-func path_clean(path : String):
+func path_clean(path : String): #prepares file paths to proper GDscript format
 	var cleaned_path : String
 	cleaned_path = path.replace("\\", "/")
 	if cleaned_path.ends_with("/"):
@@ -30,43 +32,59 @@ func path_clean(path : String):
 		cleaned_path.erase(0)
 	return cleaned_path
 
-func join_paths(root_path : String, folder : String):
+func text_clean(text : String): #takes strings and makes them acceptable for java
+	var temp_n = "" 
+	text = text.to_lower()
+	for char in text:
+		if (char >= "a" and char <= "z") or (char >= "0" and char <= "9") or char == "_":
+			temp_n += char
+		if char == " " or char == "-":
+			temp_n += "_"
+		else:
+			pass
+	return temp_n
+
+func join_paths(root_path : String, folder : String): #joins two values to a correct file path
 	var new_path : String
 	new_path = root_path + "/" + folder
 	return new_path
 
-func new_pack_setup(directory : String):
+func new_pack_setup(directory : String) -> void: #Build a new pack
 	directory_root = directory
 	report_file_path = join_paths(directory_root, "TRE.S_PACK_HELPER_REPORT.txt")
+	instructions_file_path = join_paths(directory_root, "INSTRUCTIONS.txt")
 	jsons_root = join_paths(directory_root, "jsons")
 	textures_root = join_paths(directory_root, "textures")
 	DirAccess.make_dir_absolute(directory_root)
 	setup_report()
-	report("I set up a new main pack folder at " + directory_root)
+	set_up_instructions()
 	make_folder(jsons_root)
 	make_folder(textures_root)
+	report("I finished setting up a new main pack folder at " + directory_root)
 
-func old_pack_setup(directory : String):
-	print("I opened the following directory: ", directory)
+func old_pack_setup(directory : String) -> void: #Open an existing pack
 	directory_root = directory
 	report_file_path = join_paths(directory_root, "TRE.S_PACK_HELPER_REPORT.json")
 	jsons_root = join_paths(directory_root, "jsons/")
 	textures_root = join_paths(directory_root, "textures/")
+	setup_report()
 	if !check_folder(jsons_root):
 		make_folder(jsons_root)
-		print("I made a new folder at ", jsons_root)
 	if !check_folder(textures_root):
 		make_folder(textures_root)
-		print("I made a new folder at ", textures_root)
-	print("I completed setting up the directory roots")
+	if check_folder(join_paths(jsons_root, "coats")) && check_folder(join_paths(textures_root, "coats")):
+		coats_setup = true
+	if check_folder(join_paths(jsons_root, "tack")) && check_folder(join_paths(textures_root, "tack")):
+		tack_setup = true
+	report("I opened the following directory: " + directory)
 
-func check_folder(path : String):
+func check_folder(path : String): #does a folder exist
 	if DirAccess.open(path):
 		return true
 	else:
 		return false
 
-func make_folder(path : String) -> void:
+func make_folder(path : String) -> void: #make a new folder and log it
 	DirAccess.make_dir_absolute(path)
 	report("I made a new folder at " + path)
 
@@ -77,6 +95,7 @@ func setup_coats() -> void:
 		make_folder(temp_j)
 	if !check_folder(temp_t):
 		make_folder(temp_t)
+	coats_setup = true
 
 func setup_main_tack() -> void:
 	var temp_j = join_paths(jsons_root, "tack")
@@ -85,6 +104,7 @@ func setup_main_tack() -> void:
 		make_folder(temp_j)
 	if !check_folder(temp_t):
 		make_folder(temp_t)
+	tack_setup = true
 
 func setup_tack_type(tack_type : int):
 	var temp_j = join_paths(jsons_root, "tack")
@@ -125,12 +145,12 @@ func setup_report() -> void:
 		var file = FileAccess.open(report_file_path, FileAccess.WRITE)
 		if !file:
 			ErrorManager.is_error = true
-			ErrorManager.error_print("File Location listed is not a real location. Make sure it includes the \
-			Drive letter ie: C:\\" )
+			ErrorManager.error_print("Unable to set up the report document - check the file path location." )
 			return
 		else:
 			file.store_string("This document will log all creations made by Tre's Pack Helper application.\n\n" )
 			file.close()
+	report("I set up the report document.")
 
 func report(reporting_text : String):
 	if ErrorManager.is_error:
@@ -140,10 +160,42 @@ func report(reporting_text : String):
 		var report_string = Time.get_datetime_string_from_system(false, true) + " -- " + reporting_text + "\n"
 		if !file:
 			ErrorManager.is_error = true
-			ErrorManager.error_print("File Location listed is not a real location. Make sure it includes the \
-			Drive letter ie: C:\\" )
+			ErrorManager.error_print("Unable to write a report. Please check to see if the folder pathway still exists." )
 			return
 		else:
 			file.seek_end()
 			file.store_string(report_string)
+			file.close()
+
+func set_up_instructions() -> void:
+	if ErrorManager.is_error:
+		return
+	else:
+		var file = FileAccess.open(instructions_file_path, FileAccess.WRITE)
+		if !file:
+			ErrorManager.is_error = true
+			ErrorManager.error_print("Unable to set up the instructions document - check the file path location.")
+			return
+		else:
+			file.store_string("This document will tell you the names of each texture to add and where to add them.\n \
+			Please understand, your pack will not function if the textures are not correctly named or placed in the wrong folders.\n\n" )
+			file.close()
+	report("I set up the INSTRUCTIONS.txt document")
+
+func instructions(item : String, texture_name : String, path : String):
+	if ErrorManager.is_error:
+		return
+	else:
+		var file = FileAccess.open(instructions_file_path, FileAccess.READ_WRITE)
+		var string_1 = "ADD NEW " + item.to_upper() + "\n"# ADD NEW COAT
+		var string_2 = "~~Name the Texture: " + texture_name + ".png" + "\n"# ~~Name Texture: kiwi_wonder_pony.png
+		var string_3 = "~~Save Texture in: " + path + "\n"# ~~Save to: folder/path/way
+		var instruction_string = string_1 + string_2 + string_3
+		if !file:
+			ErrorManager.is_error = true
+			ErrorManager.error_print("Unable to save instructions. Please check to see if the folder pathway still exists." )
+			return
+		else:
+			file.seek_end()
+			file.store_string(instruction_string)
 			file.close()
