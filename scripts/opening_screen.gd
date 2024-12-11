bfe3934874156c9c48fd91c2b36f68163d65112e
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-12-10 22:37:16 -0700

    connecting buttons

--- scripts/opening_screen.gd
+++ scripts/opening_screen.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 	$TextureRect.self_modulate = Color (1, 1, 1, 1)
 	$TextureRect.visible = true
 	$Timer.start(1)
-	print("starting timer")
+	%confirmButton.disabled = true
+	#print("starting timer")
 
 func _on_back_button_pressed() -> void:
 	get_tree().quit()
@@ -21,13 +22,13 @@ func _on_check_box_pressed() -> void:
 	if agreed:
 		agreed = false
 		%confirmButton.disabled = true
-		$NinePatchRect/HBoxContainer/continue.add_theme_color_override("font_color", Color(0.50, 0.51, 0.36))
+		$NinePatchRect/continue.add_theme_color_override("font_color", Color(0.50, 0.51, 0.36))
 	else:
 		agreed = true
 		%confirmButton.disabled = false
-		$NinePatchRect/HBoxContainer/continue.add_theme_color_override("font_color", Color(0.28, 0.46, 0.16))
+		$NinePatchRect/continue.add_theme_color_override("font_color", Color(0.28, 0.46, 0.16))
 
 
 func _on_timer_timeout() -> void:
-	print("starting animation")
+	#print("starting animation")
 	$AnimationPlayer.play("fade")
