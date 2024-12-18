5856465c1d75f6b495aab40b6da046ef12d37a6b
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-12-17 21:34:03 -0700

    Add credits page to the help screen

--- scripts/help_screen.gd
+++ scripts/help_screen.gd
@@ -2,22 +2,33 @@ extends Control
 
 func _ready() -> void:
 	$".".visible = true
-	$NinePatchRect.visible = false
+	$FAQ.visible = false
+	$CREDITS.visible = false
 	ErrorManager.error_alert.connect(on_error)
 	ErrorManager.error_passed.connect(on_error_passed)
 
 
 func _on_texture_button_pressed() -> void:
-	$AnimationPlayer.play("open")
+	$AnimationPlayer.play("open_faq")
 
 
 func _on_back_button_pressed() -> void:
-	$AnimationPlayer.play("exit")
+	$AnimationPlayer.play("exit_faq")
 
 func on_error() -> void:
 	$TextureButton.disabled = true
 	%backButton.disabled = true
+	%creditsbackButton.disabled = true
+	%creditsbutton.set_disabled()
 
 func on_error_passed() -> void:
 	$TextureButton.disabled = false
 	%backButton.disabled = false
+	%creditsbackButton.disabled = false
+	%creditsbutton.reenable_button()
+
+func _on_creditsbutton_button_pressed() -> void:
+	$AnimationPlayer.play("open_credits")
+
+func _on_creditsback_button_pressed() -> void:
+	$AnimationPlayer.play("exit_credits")
