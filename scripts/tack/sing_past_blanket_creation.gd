ba2d096b0f0761d3cd52d5469190719d13e2be6d
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-12-20 22:11:55 -0700

    update buttons and change node order

--- scripts/tack/sing_past_blanket_creation.gd
+++ scripts/tack/sing_past_blanket_creation.gd
@@ -54,13 +54,14 @@ func _ready() -> void:
 	if GlobalScripts.artist != "":
 		%artistText.text = GlobalScripts.artist
 		artist = true
-		ready_to_save()
+	ready_to_save()
 
 func on_error() -> void:
 	$popUPload.stop_loading()
 	disable_interaction()
 
 func on_error_continue() -> void:
+	$popUPload.stop_loading()
 	enable_interaction()
 
 func _on_back_button_pressed() -> void:
@@ -70,8 +71,8 @@ func _on_back_button_pressed() -> void:
 		get_tree().change_scene_to_file("res://scene/tackMenuGUI.tscn")
 
 func disable_interaction() -> void:
-	%confirmButton.disabled = true
-	%backButton.disabled = true
+	%confirmButton.set_disabled()
+	%backButton.set_disabled()
 	%artistText.editable = false
 	%inspoText.editable = false
 	%tackText.editable = false
@@ -89,8 +90,8 @@ func disable_interaction() -> void:
 	%custCheckBox.disabled = true
 
 func enable_interaction() -> void:
-	%confirmButton.disabled = false
-	%backButton.disabled = false
+	%confirmButton.reenable_button()
+	%backButton.reenable_button()
 	%artistText.editable = true
 	%inspoText.editable = true
 	%tackText.editable = true
@@ -171,9 +172,9 @@ func _on_coin_options_item_selected(index: int) -> void:
 
 func ready_to_save() -> void:
 	if artist && set_name && set_coin:
-		%confirmButton.disabled = false
+		%confirmButton.reenable_button()
 	else:
-		%confirmButton.disabled = true
+		%confirmButton.set_disabled()
 
 func _on_cust_check_box_pressed() -> void:
 	if custom_rack:
